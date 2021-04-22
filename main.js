3e31784b471f480bebf6ffa24f0e0f5bb51aa1e4
garzlolz <oscar861213@gmail.com>
2021-04-22 16:47:26 +0800

    done

--- main.js
+++ main.js
@@ -1,68 +1,60 @@
-
 //請求post
 var xhr = new XMLHttpRequest();
 xhr.open('post','https://data.epa.gov.tw/api/v1/aqx_p_02?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json',true);
 xhr.send();
 xhr.onload = function(){
   var data = JSON.parse(xhr.responseText).records;
-  main(data)
+  filterData(data);
 }
-    function main(data){
-      /**************************************************  選擇器  ************************************************* */
-      var select_City = document.querySelector('#city');  // 選擇器：縣市
-      var select_Town = document.querySelector('#town');  // 選擇器：城鎮
-      var title_City = document.querySelector('#Title_City'); //Content 標題：縣市
-      var title_Town = document.querySelector('#Title_Town');  //Content 標題：城鎮
-      var Content_pm = document.querySelector('.result');     //結果
-      var UpdateTime = document.querySelector('#updateTime'); //更新時間
-      /************************************************************************************************************ */
 
-
-      UpdateTime.innerHTML = data[0].DataCreationDate; // 更新時間
-      
-
-      let String1 = '';                                 //篩選縣市
-      let temp1 = [];
+function filterData(data){
+//篩選城市，綁定option
+  document.querySelector('#updateTime').textContent = data[0].DataCreationDate;
+  var Repeatcouty=[];
+  for(let i=0;i<data.length;i++){
+    Repeatcouty.push(data[i].county)
+  }
+  var County = Repeatcouty.filter(function(el,index,arr){
+    return arr.indexOf(el) === index;
+  })
+  let String = '';
+  let location = document.querySelector('#location');
+  for(let i=0;i<County.length;i++){
+    String += `
+    <option dataset="index-${i}" value="${County[i]}">${County[i]}</option>
+    `;
+    location.innerHTML = String;
+  }
+    location.addEventListener('change',function(){
+      let title = document.querySelector('#TitleCountry');
+      var String = ''
       for(let i=0;i<data.length;i++){
-        temp1.push(data[i].county);
-      }
-      var temp1_Result = temp1.filter(function(el,index,arr){
-        return arr.indexOf(el) === index;
-      })
-      for(let i=0;i<temp1_Result.length;i++){
-        String1 += `<option dataset="${i}" value="${temp1_Result[i]}">${temp1_Result[i]}</option>`
-      }
-      select_City.innerHTML = String1;                //綁定選擇器 : 縣市
-
-      select_City.addEventListener('change',function(){ //縣市添加event
-
-        let String = ' '
-        for(let i=0;i<data.length;i++){
-          if(select_City.value == data[i].county){
-            String += `<option dataset="index${i}" value="${data[i].Site}">${data[i].Site}</option>`
-          }
+        if(location.value == data[i].county){
+          String += 
+          `<option dataset="index-#{i}" value="${data[i].Site}">${data[i].Site}</option>`
         }
-        select_Town.innerHTML = String;
-        chagetitle();
-      })
-      select_Town.addEventListener('change',chagetitle,false)//城鎮添加event
-
-       
-      function chagetitle(){                        //改變title
-        title_City.textContent = select_City.value;
-        title_Town.textContent = select_Town.value;
-        
-        let string = ' '
-        for(let i=0;i<data.length;i++){
-          if(data[i].county == select_City.value && data[i].Site == select_Town.value){
-            string+= `
-              <div>${data[i].county},${data[i].Site}, PM2.5: ${data[i].PM25} ${data[i].ItemUnit}</div>
-            `
-          }
-        }
-        Content_pm.innerHTML = string;
       }
+      title.textContent = location.value;
 
-      
+      let ContString = document.querySelector('.info');
+      let string2 = '';
+      for(let i=0;i<data.length;i++){
+        if(title.textContent == data[i].county){
+          string2 += `<div class ="info${i}">
+              <h3>${data[i].county},${data[i].Site}監測站</h3> 
+              <h5>
+                          更新時間 : <em>${data[i].DataCreationDate}</em>
+                    <p>PM2.5 : ${data[i].PM25}  ${data[i].ItemUnit}</p>
+              </h5>
+          </div>`
+        }
+      }
+      ContString.innerHTML = string2;
 
-    }
+
+
+
+    },false)
+
+
+}
